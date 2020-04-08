073ed0bf29cfede34c52cddd5daeaf50faf94cee
kunal <kunalm039@gmail.com>
2020-04-08 17:41:27 +0530

    just testing nvm

---
+++ Test.py
@@ -0,0 +1,4 @@
+for i in range(1,10):
+    xpathh = "/html/body/main/article/form/div[1]/table/tbody/tr[2]/td/div[2]/select/option[%s]" %i
+    print(xpathh)
+    /html/body/main/article/form/div[1]/table/tbody/tr[2]/td/div[2]/select/option[549]
